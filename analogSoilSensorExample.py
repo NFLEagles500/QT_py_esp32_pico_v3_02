679c4dc7b79d9641e3a68e5de09805c94d8a422e
NFLEagles500 <NFLEagles500@gmail.com>
2024-10-16 12:12:02 -0600

    Update analogSoilSensorExample.py

--- analogSoilSensorExample.py
+++ analogSoilSensorExample.py
@@ -2,14 +2,14 @@ import requests
 import machine
 import time
 
-adc = machine.ADC(26)
+adc = machine.ADC(33,atten=machine.ADC.ATTN_11DB)
 
 # Test the soil meter with a cup of water to get x_max value.  Notice that the
 #max value is LOWER than the min value.  Set the x_min value based on what the
 #meter reads when its just in the air.  #Use the additional plus/minus value in
 #x_min to adjust the reading closer to what a moisture meter says
-x_min = 24565 - 4700
-x_max = 13800
+x_min = 49000 -12000
+x_max = 20200
 y_min = 0
 y_max = 100
 
@@ -27,4 +27,6 @@ def linear_scale(x, x_min, x_max, y_min, y_max):
 while True:
     result = linear_scale(adc.read_u16(), x_min, x_max, y_min, y_max)
     print(result)
+    print(adc.read_u16())
     time.sleep(1)
+
